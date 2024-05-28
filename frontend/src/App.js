8d1fafc47e35fba85fb41ac4ea9cedf5095b295a
l-suyeon-l <stellasue@naver.com>
2024-05-28 20:45:43 +0900

    05280845

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
-<<<<<<< HEAD
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Findpage1 from './Component/Findpage1';
 import Findpage2 from './Component/Findpage2';
 import Umbenroll2 from './Component/Umbenroll2';
+import StoreInfo from './Component/StoreInfo';
 // import Home from './Component/Home';
 // import Login from './Component/Login.jsx'
 
-=======
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Findpage1 from './Component/Findpage1';
-import Home from './Component/Home';
-import Findpage1 from './Component/Findpage1';
-import StoreInfo from './Component/StoreInfo';
->>>>>>> a273194352c0a5876eecb788815798deef1582a1
+
 import './App.css';
 
 function App() {
@@ -26,13 +21,7 @@ function App() {
     //     </Routes>
     //   </div>
     // </Router>
-<<<<<<< HEAD
-    <Umbenroll2 />
-=======
-    <Home />
-    // <Findpage1/>
-    // <StoreInfo />
->>>>>>> a273194352c0a5876eecb788815798deef1582a1
+    <StoreInfo />
   );
 }
 
---
+++ frontend/src/Component/Umbenroll1.jsx
@@ -0,0 +1,70 @@
+import React from "react"; 
+import "./style.css";  
+
+export const Screen = () => {   
+    return (     
+        <div className="screen">       
+            <div className="div">         
+                <div className="background">           
+                    <div className="view">             
+                        <div className="view-2">               
+                            <div className="div-wrapper">                 
+                                <p className="text-wrapper">현재 가게에서 보유하고 있는 우산 개수를 입력해주세요</p>               
+                            </div>               
+                            <div className="text-wrapper-2">우산 재고</div>               
+                            <div className="view-3">                 
+                                <div className="text-wrapper-3">확인</div>               
+                            </div>             
+                        </div>             
+                        <div className="overlap-group">               
+                            <div className="view-4">                 
+                                <div className="div-2">                   
+                                    <div className="text-wrapper-4">주소 :</div>                   
+                                    <p className="p">대구 북구 대학로23길 114 1층</p>                 
+                                </div>                 
+                                <div className="div-3">                   
+                                    <div className="text-wrapper-4">상호명 :</div>                   
+                                    <div className="text-wrapper-5">벨로</div>                 
+                                </div>              
+                            </div>               
+                            <div className="div-4">                 
+                                <div className="text-wrapper-6">님의 가게 정보</div>                 
+                                <div className="text-wrapper-7">김원빈</div>               
+                            </div>             
+                        </div>           
+                    </div>           
+                    <div className="view-5">             
+                        <div className="text-wrapper-8">우산 재고 등록</div>           
+                    </div>         
+                </div>         
+                <div className="navigator-login">           
+                    <div className="view-6">             
+                        <div className="view-7">               
+                            <div className="text-wrapper-9">마이페이지</div>             
+                        </div>             
+                        <div className="view-8">               
+                            <div className="text-wrapper-9">로그아웃</div>             
+                        </div>           
+                    </div>           
+                    <div className="view-9">             
+                        <div className="element">               
+                            <div className="text-wrapper-10">공지사항</div>             
+                        </div>             
+                        <div className="element-2">               
+                            <div className="text-wrapper-11">가게 정보 등록</div>             
+                        </div>             
+                        <div className="element-3">               
+                            <div className="text-wrapper-12">우산 재고 등록</div>             
+                        </div>             
+                        <div className="element-4">               
+                            <div className="text-wrapper-12">우산 찾기</div>             
+                        </div>           
+                    </div>           
+                    <div className="view-10">             
+                        <div className="text-wrapper-13">BP해</div>           
+                    </div>         
+                </div>       
+            </div>     
+        </div>   
+    ); 
+};
