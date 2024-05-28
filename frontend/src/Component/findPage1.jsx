4bb0e2118646c5eabecede0f843347b5431f65cb
l-suyeon-l <stellasue@naver.com>
2024-05-28 13:47:52 +0900

    findPage1 추가

---
+++ frontend/src/Component/findPage1.jsx
@@ -0,0 +1,159 @@
+import React from "react";
+import "./style.css";
+
+export const Element = () => {
+  return (
+    <div className="element">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <div className="map" />
+          <div className="pop-up">
+            <div className="store-list">
+              <img className="image" alt="Image" src="image.svg" />
+              <div className="div">
+                <div className="overlap-group-wrapper">
+                  <div className="overlap-group">
+                    <div className="div-wrapper">
+                      <div className="text-wrapper">9개</div>
+                    </div>
+                    <div className="text-wrapper-2">현재 보유 우산</div>
+                  </div>
+                </div>
+                <div className="div-wrapper-2">
+                  <div className="text-wrapper-3">070-8875-7777</div>
+                </div>
+                <div className="div-wrapper-3">
+                  <div className="text-wrapper-4">(지번) 산격동 1325-7</div>
+                </div>
+                <div className="div-wrapper-4">
+                  <div className="text-wrapper-5">(우)41544</div>
+                </div>
+                <div className="div-wrapper-5">
+                  <p className="text-wrapper-3">대구 북구 대학로23길 13 1층</p>
+                </div>
+                <div className="div-wrapper-6">
+                  <div className="text-wrapper-6">비블리오테카</div>
+                </div>
+                <img className="img" alt="Image" src="image-6.svg" />
+                <img className="img-2" alt="Element" src="1-3.png" />
+              </div>
+              <div className="div-2">
+                <div className="overlap-group-wrapper">
+                  <div className="overlap-group">
+                    <div className="div-wrapper">
+                      <div className="text-wrapper">6개</div>
+                    </div>
+                    <div className="text-wrapper-2">현재 보유 우산</div>
+                  </div>
+                </div>
+                <div className="div-wrapper-2">
+                  <div className="text-wrapper-3">053-939-0512</div>
+                </div>
+                <div className="div-wrapper-3">
+                  <div className="text-wrapper-4">(지번) 산격동 1321-9</div>
+                </div>
+                <div className="div-wrapper-4">
+                  <div className="text-wrapper-5">(우)41544</div>
+                </div>
+                <div className="div-wrapper-5">
+                  <div className="text-wrapper-3">대구 북구 대학로23길 9</div>
+                </div>
+                <div className="div-wrapper-6">
+                  <div className="text-wrapper-6">어썸브루커피</div>
+                </div>
+                <img className="image-2" alt="Image" src="image-5.svg" />
+                <img className="img-3" alt="Element" src="1-2.png" />
+              </div>
+              <div className="div-3">
+                <img className="img-3" alt="Element" src="1.png" />
+                <div className="overlap-group-wrapper">
+                  <div className="overlap-group">
+                    <div className="div-wrapper">
+                      <div className="text-wrapper">0개</div>
+                    </div>
+                    <div className="text-wrapper-2">현재 보유 우산</div>
+                  </div>
+                </div>
+                <div className="div-wrapper-2">
+                  <div className="text-wrapper-3">053-264-7442</div>
+                </div>
+                <div className="div-wrapper-3">
+                  <div className="text-wrapper-4">(지번) 산격동 1309-15</div>
+                </div>
+                <div className="div-wrapper-4">
+                  <div className="text-wrapper-5">(우)41544</div>
+                </div>
+                <div className="div-wrapper-5">
+                  <p className="text-wrapper-3">대구 북구 대학로23길 18-9 1층</p>
+                </div>
+                <div className="div-wrapper-6">
+                  <div className="text-wrapper-6">낭만놀이</div>
+                </div>
+                <img className="image-2" alt="Image" src="image-3.svg" />
+              </div>
+              <div className="div-4">
+                <img className="img-3" alt="Image" src="image-2.png" />
+                <div className="overlap-group-wrapper">
+                  <div className="overlap-group">
+                    <div className="div-wrapper">
+                      <div className="text-wrapper">2개</div>
+                    </div>
+                    <div className="text-wrapper-2">현재 보유 우산</div>
+                  </div>
+                </div>
+                <div className="div-wrapper-2">
+                  <div className="text-wrapper-3">0507-1414-3994</div>
+                </div>
+                <div className="div-wrapper-3">
+                  <div className="text-wrapper-4">(지번) 산격동 1308-22</div>
+                </div>
+                <div className="div-wrapper-4">
+                  <div className="text-wrapper-5">(우)41544</div>
+                </div>
+                <div className="div-wrapper-5">
+                  <p className="text-wrapper-3">대구 북구 대학로23길 114 1층</p>
+                </div>
+                <div className="div-wrapper-6">
+                  <div className="text-wrapper-6">벨로</div>
+                </div>
+                <img className="image-2" alt="Image" src="image-4.svg" />
+              </div>
+            </div>
+            <div className="search">
+              <img className="image-3" alt="Image" src="image.png" />
+              <img className="image-4" alt="Image" src="image-2.svg" />
+              <div className="text-wrapper-7">상호명 및 주소를 검색해주세요</div>
+            </div>
+          </div>
+          <div className="navigator">
+            <div className="view">
+              <div className="view-2">
+                <div className="text-wrapper-8">회원가입</div>
+              </div>
+              <div className="view-3">
+                <div className="text-wrapper-8">로그인</div>
+              </div>
+            </div>
+            <div className="view-4">
+              <div className="div-wrapper-7">
+                <div className="text-wrapper-9">공지사항</div>
+              </div>
+              <div className="div-wrapper-8">
+                <div className="text-wrapper-10">가게 정보 등록</div>
+              </div>
+              <div className="div-wrapper-9">
+                <div className="text-wrapper-11">우산 재고 등록</div>
+              </div>
+              <div className="div-wrapper-10">
+                <div className="text-wrapper-11">우산 찾기</div>
+              </div>
+            </div>
+            <div className="view-5">
+              <div className="text-wrapper-12">BP해</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
